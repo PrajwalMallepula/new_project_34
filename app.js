1d1b6c98a99e089d688abeca3b10946fa941bfa5
Prajwal_Mallepula <149990416+PrajwalMallepula@users.noreply.github.com>
2026-03-25 17:40:00 +0530

    Add category router to the Express app

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const productRouter = require("./products");
-
+const categoryRouter = require("./categorys");
+//const userRouter = require("./users");
 const port = 5000;
 require('dotenv').config();
 const parser = require("body-parser");
@@ -12,7 +13,7 @@ app.use(morgan("tiny"));
 
 const api = process.env.API_URL;
 app.use(`${api}/products`,productRouter);
-
+app.use(`${api}/category`,categoryRouter);
 mongoose.connect(process.env.CONNECTION_API).then(()=>{
        console.log("db connected sucesfully");
 })
